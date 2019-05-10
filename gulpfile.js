20862ea0565dd9b577b4c08e656703f3afb423d2
Andrii Mulyk <amulyk@lectrum.io>
2019-05-10 12:44:40 +0400

    feat(Create a basic structure):

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del = require('del');
+ 
+sass.compiler = require('node-sass');
+
+gulp.task('clean:css', () => {
+  return del([
+      'dist/css/*',
+  ]);
+});
+
+gulp.task('clean:html', () => {
+    return del([
+        'dist/*.html',
+    ]);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('source/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('html', function(){
+    return gulp.src('source/*.html')
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('source/*.html', (done) => {
+        gulp.series(['clean:html', 'html'])(done);
+    });
+    gulp.watch('source/styles/*.scss', (done) => {
+        gulp.series(['clean:css', 'sass'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['watch']));
